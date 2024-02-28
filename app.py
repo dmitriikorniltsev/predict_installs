f630d83b1aabf07558983ab0df93a9ef38f52882
dmitriikorniltsev <dmitriikorniltsev@gmail.com>
2024-02-28 22:46:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from src.data_model import ModelFeatures
 
 
 filename = 'data/model/xgb_model_top_10.sav'
+model = pickle.load(open(filename, 'rb'))
 
-with open(filename, "rb") as f:
-    model = pickle.load(f)
+# with open(filename, "rb") as f:
+#     model = pickle.load(f)
 
 app = FastAPI()
 
@@ -16,7 +17,30 @@ app = FastAPI()
 @app.get("/api/test")
 async def test():
     return "Hello World!"
+
+@app.get("/api/test_predict")
+async def test():
+    test_data = {
+        'Heidelberg': 86.93,
+        'Essen': 0.0,
+        'Dortmund': 0.0,
+        'Ulm': 801.76,
+        'Bergedorf': 0.0,
+        'Wuerzburg': 9.89,
+        'Bottrop': 4.1721,
+        'Darmstadt': 828.59,
+        'Goettingen': 0.0,
+        'Recklinghausen': 0.0
+    }
+    input_data = np.array(list(ModelFeatures(**test_data).model_dump().values())).reshape(1, -1)
     
+    # Perform prediction using the loaded model
+    predicted_installs = model.predict(input_data)
+    
+    return {
+        'predicted_installs': predicted_installs.tolist()
+    }
+   
 @app.get('/api/predict_v2')
 async def predict():
     features = {
